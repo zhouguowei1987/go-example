5d59d66b849edb3bf067630a3cd1cf672eca21e0
729044963@qq.com <729044963@qq.com>
2022-09-30 10:54:46 +0800

    time

--- 45.string-functions.go
+++ 45.string-functions.go
@@ -5,22 +5,22 @@ import (
 	s "strings"
 )
 
-var p = fmt.Println
+var p45 = fmt.Println
 
 func main() {
-	p("Contains:", s.Contains("test", "t"))
-	p("Count:", s.Count("test", "t"))
-	p("HasPrefix:", s.HasPrefix("test", "te"))
-	p("HasSuffix:", s.HasSuffix("test", "st"))
-	p("Index:", s.Index("test", "e"))
-	p("Join:", s.Join([]string{"a", "b"}, "-"))
-	p("Repeat:", s.Repeat("a", 5))
-	p("Replace:", s.Replace("foo", "o", "0", -1))
-	p("Replace:", s.Replace("foo", "o", "0", 1))
-	p("Split:", s.Split("a-b-c-d-e", "-"))
-	p("ToLower:", s.ToLower("TEST"))
-	p("ToUpper", s.ToUpper("test"))
+	p45("Contains:", s.Contains("test", "t"))
+	p45("Count:", s.Count("test", "t"))
+	p45("HasPrefix:", s.HasPrefix("test", "te"))
+	p45("HasSuffix:", s.HasSuffix("test", "st"))
+	p45("Index:", s.Index("test", "e"))
+	p45("Join:", s.Join([]string{"a", "b"}, "-"))
+	p45("Repeat:", s.Repeat("a", 5))
+	p45("Replace:", s.Replace("foo", "o", "0", -1))
+	p45("Replace:", s.Replace("foo", "o", "0", 1))
+	p45("Split:", s.Split("a-b-c-d-e", "-"))
+	p45("ToLower:", s.ToLower("TEST"))
+	p45("ToUpper", s.ToUpper("test"))
 
-	p("Len:", len("hello"))
-	p("Char", "hello"[1])
+	p45("Len:", len("hello"))
+	p45("Char", "hello"[1])
 }
