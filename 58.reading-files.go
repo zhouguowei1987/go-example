02094a73eac826712f8bed0bd9b04f91c516e7f9
729044963@qq.com <729044963@qq.com>
2022-10-09 10:12:18 +0800

    writing-files

--- 58.reading-files.go
+++ 58.reading-files.go
@@ -8,7 +8,7 @@ import (
 	"os"
 )
 
-func check(e error) {
+func check58(e error) {
 	if e != nil {
 		panic(e)
 	}
@@ -16,37 +16,37 @@ func check(e error) {
 
 func main() {
 	dat, err := ioutil.ReadFile("dat")
-	check(err)
+	check58(err)
 	fmt.Print(string(dat))
 
 	f, err := os.Open("dat")
-	check(err)
+	check58(err)
 	defer f.Close()
 
 	b1 := make([]byte, 5)
 	n1, err := f.Read(b1)
-	check(err)
+	check58(err)
 	fmt.Printf("%d bytes : %s\n", n1, string(b1))
 
 	o2, err := f.Seek(6, 0)
-	check(err)
+	check58(err)
 	b2 := make([]byte, 2)
 	n2, err := f.Read(b2)
-	check(err)
+	check58(err)
 	fmt.Printf("%d bytes @ %d:%s\n", n2, o2, string(b2))
 
 	o3, err := f.Seek(6, 0)
-	check(err)
+	check58(err)
 	b3 := make([]byte, 2)
 	n3, err := io.ReadAtLeast(f, b3, 2)
-	check(err)
+	check58(err)
 	fmt.Printf("%d bytes @ %d:%s\n", n3, o3, string(b3))
 
 	_, err = f.Seek(0, 0)
-	check(err)
+	check58(err)
 
 	r4 := bufio.NewReader(f)
 	b4, err := r4.Peek(5)
-	check(err)
+	check58(err)
 	fmt.Printf("5 bytes: %s\n", string(b4))
 }
