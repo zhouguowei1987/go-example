1277982fdccc57d4f28b7d8cf3a148f8103c117a
729044963@qq.com <729044963@qq.com>
2023-04-12 15:25:01 +0800

    暂停20秒

--- 96.tikuvip.51test.net.go
+++ 96.tikuvip.51test.net.go
@@ -351,7 +351,7 @@ func PathList(path string) (tiKuVip51TestPathListDataFileList []TiKuVip51TestPat
 }
 
 var downloadNumber = 0
-var sleepSecond = 10
+var sleepSecond = 20
 
 func tiKuVip51TestDownloadUrl(tiKuVip51TestPathListDataFileList []TiKuVip51TestPathListDataFileList) {
 	for _, pathListDataFile := range tiKuVip51TestPathListDataFileList {
