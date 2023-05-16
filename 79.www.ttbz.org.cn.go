bb1f41412518e7eceff6dbb842053cfca5437b09
周国伟 <zhouguowei@zhouguoweideMacBook-Pro.local>
2023-05-16 10:28:30 +0800

    完结

--- 79.www.ttbz.org.cn.go
+++ 79.www.ttbz.org.cn.go
@@ -17,8 +17,8 @@ import (
 // @Title 获取全国团体标准信息平台Pdf文档
 // @Description http://www.ttbz.org.cn/，将全国团体标准信息平台Pdf文档入库
 func main() {
-	var startId = 80917
-	var endId = 81023
+	var startId = 81901
+	var endId = 81956
 	var id = startId
 	var isGoGo = true
 	for isGoGo {
