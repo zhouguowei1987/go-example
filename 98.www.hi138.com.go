1da69466f49de5e9250d2b5f738fc18f67a5dc9d
729044963@qq.com <729044963@qq.com>
2023-06-06 10:58:20 +0800

    完结

--- 98.www.hi138.com.go
+++ 98.www.hi138.com.go
@@ -106,7 +106,7 @@ func main() {
 										}
 										fmt.Println("=======开始完成========")
 									}
-									time.Sleep(time.Second * 1)
+									time.Sleep(time.Millisecond * 100)
 									break
 								}
 							}
