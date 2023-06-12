d9cad5e943043e51c2f0c38929d8fb735edb46ff
周国伟 <zhouguowei@zhouguoweideMacBook-Pro.local>
2023-06-12 09:25:07 +0800

    团体标准

--- 79.www.ttbz.org.cn.go
+++ 79.www.ttbz.org.cn.go
@@ -17,8 +17,8 @@ import (
 // @Title 获取全国团体标准信息平台Pdf文档
 // @Description http://www.ttbz.org.cn/，将全国团体标准信息平台Pdf文档入库
 func main() {
-	var startId = 21027
-	var endId = 83291
+	var startId = 83291
+	var endId = 83492
 	goCh := make(chan int, endId-startId)
 	for id := startId; id <= endId; id++ {
 		go func(id int) {
