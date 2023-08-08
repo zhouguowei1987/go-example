e9e53c691322de8480dca8e444266e6f10ee49a5
zhouguowei <729044963@qq.com>
2023-08-08 13:32:30 +0800

    更改目录名称

--- 79.www.ttbz.org.cn.go
+++ 79.www.ttbz.org.cn.go
@@ -17,8 +17,8 @@ import (
 // @Title 获取全国团体标准信息平台Pdf文档
 // @Description http://www.ttbz.org.cn/，将全国团体标准信息平台Pdf文档入库
 func main() {
-	var startId = 80000
-	var endId = 87151
+	var startId = 87151
+	var endId = 87281
 	goCh := make(chan int, endId-startId)
 	for id := startId; id <= endId; id++ {
 		go func(id int) {
