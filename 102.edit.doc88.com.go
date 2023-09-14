049f4a5656ecf29097ea5102123e36655a06a690
zhouguowei <729044963@qq.com>
2023-09-14 22:49:02 +0800

    每个暂停2秒

--- 102.edit.doc88.com.go
+++ 102.edit.doc88.com.go
@@ -76,7 +76,7 @@ var EditCount = 1
 // @Title 编辑道客巴巴文档
 // @Description https://www.doc88.com/，编辑道客巴巴文档
 func main() {
-	curPage := 1
+	curPage := 150
 	for {
 		pageListUrl := fmt.Sprintf("https://www.doc88.com/uc/doc_manager.php?act=ajax_doc_list&curpage=%d", curPage)
 		fmt.Println(pageListUrl)
@@ -211,6 +211,7 @@ func main() {
 			}
 		}
 		curPage++
+		time.Sleep(time.Second * 2)
 	}
 }
 
