5df2c6133384571ea0c9f6d6099fa1f4cc917afb
zhouguowei <729044963@qq.com>
2023-10-19 17:11:42 +0800

    生态环境标准

--- 110.www.mee.gov.cn.go
+++ 110.www.mee.gov.cn.go
@@ -141,6 +141,9 @@ func main() {
 									chineseTitle = strings.ReplaceAll(chineseTitle, "/", "-")
 									chineseTitle = strings.ReplaceAll(chineseTitle, " ", "")
 									chineseTitle = strings.ReplaceAll(chineseTitle, "：", ":")
+									chineseTitle = strings.ReplaceAll(chineseTitle, "—", "-")
+									chineseTitle = strings.ReplaceAll(chineseTitle, "（", "(")
+									chineseTitle = strings.ReplaceAll(chineseTitle, "）", ")")
 									fmt.Println(chineseTitle)
 
 									// 下载文档URL
