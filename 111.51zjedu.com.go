6eb0cd7c20a43deca3655b41ee4002e22dd92cc5
729044963@qq.com <729044963@qq.com>
2023-10-31 11:48:19 +0800

    downloadZJEduView

--- 111.51zjedu.com.go
+++ 111.51zjedu.com.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/antchfx/htmlquery"
+	"golang.org/x/net/html"
 	"io"
 	"net"
 	"net/http"
@@ -126,7 +127,7 @@ func main() {
 				downloadViewUrl := "http://www.51zjedu.com" + string(regDownloadViewUrlMatch[0][1])
 				fmt.Println(downloadViewUrl)
 
-				downloadViewDoc, err := htmlquery.LoadURL(downloadViewUrl)
+				downloadViewDoc, err := downloadZJEduView(downloadViewUrl, viewUrl)
 				if err != nil {
 					fmt.Println(err)
 					continue
@@ -137,10 +138,10 @@ func main() {
 				downLoadUrl = "http://www.51zjedu.com/e/DownSys" + downLoadUrl
 				fmt.Println(downLoadUrl)
 
-				filePath := "../www.51zjedu.com/" + grade.name + "/" + fileName
+				filePath := "../www.51zjedu.com/www.51zjedu.com/" + grade.name + "/" + fileName
 				if _, err := os.Stat(filePath); err != nil {
 					fmt.Println("=======开始下载" + strconv.Itoa(current) + "========")
-					err = downloadZJEdu(downLoadUrl, viewUrl, filePath)
+					err = downloadZJEdu(downLoadUrl, downloadViewUrl, filePath)
 					if err != nil {
 						fmt.Println(err)
 						continue
@@ -163,6 +164,70 @@ func main() {
 			isPageListGo = true
 		}
 	}
+}
+
+func downloadZJEduView(requestUrl string, referer string) (doc *html.Node, err error) {
+	// 初始化客户端
+	var client *http.Client = &http.Client{
+		Transport: &http.Transport{
+			Dial: func(netw, addr string) (net.Conn, error) {
+				c, err := net.DialTimeout(netw, addr, time.Second*3)
+				if err != nil {
+					fmt.Println("dail timeout", err)
+					return nil, err
+				}
+				return c, nil
+
+			},
+			MaxIdleConnsPerHost:   10,
+			ResponseHeaderTimeout: time.Second * 3,
+		},
+	}
+	if Tc260EnableHttpProxy {
+		client = Tc260SetHttpProxy()
+	}
+	req, err := http.NewRequest("GET", requestUrl, nil) //建立连接
+	if err != nil {
+		return doc, err
+	}
+
+	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
+	req.Header.Set("Accept-Encoding", "gzip, deflate, br")
+	req.Header.Set("Accept-Language", "zh-CN,zh;q=0.9")
+	req.Header.Set("Cache-Control", "max-age=0")
+	req.Header.Set("Connection", "keep-alive")
+	req.Header.Set("Cookie", "Hm_lvt_252de7ebbd9c14e242825bc998540b7b=1698723598; __gpi=UID=00000c7bd89bada9:T=1698723598:RT=1698723598:S=ALNI_MbfatfhOGZiAMY5Xzy6D12cOXgd_g; _gid=GA1.2.474766830.1698723601; Hm_lpvt_252de7ebbd9c14e242825bc998540b7b=1698723617; __gads=ID=eb519680500e3804-22d97d5b32e500c0:T=1698723598:RT=1698723617:S=ALNI_MYgZco8kzGtf6ailJqCfr8BOKBOBQ; ZDEDebuggerPresent=php,phtml,php3; unomsmlusername=15238369929; unomsmluserid=122753; unomsmlgroupid=1; unomsmlrnd=KJKUEPIpuNTzgAzJQiw4; unomsmlauth=6f61644e5cd3d9739fed4d4b963d2ebc; _ga=GA1.1.404381428.1698723599; _ga_34B604LFFQ=GS1.1.1698723601.1.1.1698723899.60.0.0")
+	req.Header.Set("Host", "www.51zjedu.com")
+	req.Header.Set("Origin", "http://www.51zjedu.com/")
+	req.Header.Set("Referer", referer)
+	req.Header.Set("sec-ch-ua", "\"Chromium\";v=\"118\", \"Google Chrome\";v=\"118\", \"Not=A?Brand\";v=\"99\"")
+	req.Header.Set("sec-ch-ua-mobile", "?0")
+	req.Header.Set("sec-ch-ua-platform", "\"macOS\"")
+	req.Header.Set("Sec-Fetch-Dest", "document")
+	req.Header.Set("Sec-Fetch-Mode", "cors")
+	req.Header.Set("Sec-Fetch-Site", "same-origin")
+	req.Header.Set("Sec-Fetch-User", "?1")
+	req.Header.Set("Upgrade-Insecure-Requests", "1")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36")
+	resp, err := client.Do(req) //拿到返回的内容
+	if err != nil {
+		return doc, err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}(resp.Body)
+	// 如果访问失败，就打印当前状态码
+	if resp.StatusCode != http.StatusOK {
+		return doc, errors.New("http status :" + strconv.Itoa(resp.StatusCode))
+	}
+	doc, err = htmlquery.Parse(resp.Body)
+	if err != nil {
+		return doc, err
+	}
+	return doc, nil
 }
 
 func downloadZJEdu(attachmentUrl string, referer string, filePath string) error {
