7ff61b1c39e3f49d40d97fa3622c33f24d92c240
zhouguowei <729044963@qq.com>
2023-11-29 16:39:24 +0800

    道客巴巴修改文档价格

--- 79.www.ttbz.org.cn.go
+++ 79.www.ttbz.org.cn.go
@@ -17,8 +17,8 @@ import (
 // @Title 获取全国团体标准信息平台Pdf文档
 // @Description https://www.ttbz.org.cn/，将全国团体标准信息平台Pdf文档入库
 func main() {
-	var startId = 95000
-	var endId = 95193
+	var startId = 95500
+	var endId = 95806
 	goCh := make(chan int, endId-startId)
 	for id := startId; id <= endId; id++ {
 		go func(id int) {
