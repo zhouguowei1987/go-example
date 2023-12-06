010463c2052ed881e323ea61b6133cd56dd9873d
729044963@qq.com <729044963@qq.com>
2023-12-06 16:25:24 +0800

    第一试卷网---小学试卷

--- 84.www.shijuan1.com.go
+++ 84.www.shijuan1.com.go
@@ -61,99 +61,99 @@ var AllTestCategory = []TestCategory{
 			"https://www.shijuan1.com/a/sjyy6/",
 		},
 	},
-	{
-		name: "中考试卷",
-		link: []string{
-			"https://www.shijuan1.com/a/sjyw7/",
-			"https://www.shijuan1.com/a/sjyw8/",
-			"https://www.shijuan1.com/a/sjyw9/",
-			"https://www.shijuan1.com/a/sjywzk/",
-
-			"https://www.shijuan1.com/a/sjsx7/",
-			"https://www.shijuan1.com/a/sjsx8/",
-			"https://www.shijuan1.com/a/sjsx9/",
-			"https://www.shijuan1.com/a/sjsxzk/",
-
-			"https://www.shijuan1.com/a/sjyy7/",
-			"https://www.shijuan1.com/a/sjyy8/",
-			"https://www.shijuan1.com/a/sjyy9/",
-			"https://www.shijuan1.com/a/sjyyzk/",
-
-			"https://www.shijuan1.com/a/sjwl8/",
-			"https://www.shijuan1.com/a/sjwl9/",
-			"https://www.shijuan1.com/a/sjwlzk/",
-
-			"https://www.shijuan1.com/a/sjhx9/",
-			"https://www.shijuan1.com/a/sjhxzk/",
-
-			"https://www.shijuan1.com/a/sjzz7/",
-			"https://www.shijuan1.com/a/sjzz8/",
-			"https://www.shijuan1.com/a/sjzz9/",
-			"https://www.shijuan1.com/a/sjzzzk/",
-
-			"https://www.shijuan1.com/a/sjls7/",
-			"https://www.shijuan1.com/a/sjls8/",
-			"https://www.shijuan1.com/a/sjls9/",
-			"https://www.shijuan1.com/a/sjlszk/",
-
-			"https://www.shijuan1.com/a/sjdl7/",
-			"https://www.shijuan1.com/a/sjdl8/",
-			"https://www.shijuan1.com/a/sjdlzk/",
-
-			"https://www.shijuan1.com/a/sjsw7/",
-			"https://www.shijuan1.com/a/sjsw8/",
-			"https://www.shijuan1.com/a/sjswzk/",
-		},
-	},
-	{
-		name: "高考试卷",
-		link: []string{
-			"https://www.shijuan1.com/a/sjywg1/",
-			"https://www.shijuan1.com/a/sjywg2/",
-			"https://www.shijuan1.com/a/sjywg3/",
-			"https://www.shijuan1.com/a/sjywgk/",
-
-			"https://www.shijuan1.com/a/sjsxg1/",
-			"https://www.shijuan1.com/a/sjsxg2/",
-			"https://www.shijuan1.com/a/sjsxg3/",
-			"https://www.shijuan1.com/a/sjsxgk/",
-
-			"https://www.shijuan1.com/a/sjyyg1/",
-			"https://www.shijuan1.com/a/sjyyg2/",
-			"https://www.shijuan1.com/a/sjyyg3/",
-			"https://www.shijuan1.com/a/sjyygk/",
-
-			"https://www.shijuan1.com/a/sjwlg1/",
-			"https://www.shijuan1.com/a/sjwlg2/",
-			"https://www.shijuan1.com/a/sjwlg3/",
-			"https://www.shijuan1.com/a/sjwlgk/",
-
-			"https://www.shijuan1.com/a/sjhxg1/",
-			"https://www.shijuan1.com/a/sjhxg2/",
-			"https://www.shijuan1.com/a/sjhxg3/",
-			"https://www.shijuan1.com/a/sjhxgk/",
-
-			"https://www.shijuan1.com/a/sjzzg1/",
-			"https://www.shijuan1.com/a/sjzzg2/",
-			"https://www.shijuan1.com/a/sjzzg3/",
-			"https://www.shijuan1.com/a/sjzzgk/",
-
-			"https://www.shijuan1.com/a/sjlsg1/",
-			"https://www.shijuan1.com/a/sjlsg2/",
-			"https://www.shijuan1.com/a/sjlsg3/",
-			"https://www.shijuan1.com/a/sjlsgk/",
-
-			"https://www.shijuan1.com/a/sjdlg1/",
-			"https://www.shijuan1.com/a/sjdlg2/",
-			"https://www.shijuan1.com/a/sjdlg3/",
-			"https://www.shijuan1.com/a/sjdlgk/",
-
-			"https://www.shijuan1.com/a/sjswg1/",
-			"https://www.shijuan1.com/a/sjswg2/",
-			"https://www.shijuan1.com/a/sjswg3/",
-			"https://www.shijuan1.com/a/sjswgk/",
-		},
-	},
+	//{
+	//	name: "中考试卷",
+	//	link: []string{
+	//		"https://www.shijuan1.com/a/sjyw7/",
+	//		"https://www.shijuan1.com/a/sjyw8/",
+	//		"https://www.shijuan1.com/a/sjyw9/",
+	//		"https://www.shijuan1.com/a/sjywzk/",
+	//
+	//		"https://www.shijuan1.com/a/sjsx7/",
+	//		"https://www.shijuan1.com/a/sjsx8/",
+	//		"https://www.shijuan1.com/a/sjsx9/",
+	//		"https://www.shijuan1.com/a/sjsxzk/",
+	//
+	//		"https://www.shijuan1.com/a/sjyy7/",
+	//		"https://www.shijuan1.com/a/sjyy8/",
+	//		"https://www.shijuan1.com/a/sjyy9/",
+	//		"https://www.shijuan1.com/a/sjyyzk/",
+	//
+	//		"https://www.shijuan1.com/a/sjwl8/",
+	//		"https://www.shijuan1.com/a/sjwl9/",
+	//		"https://www.shijuan1.com/a/sjwlzk/",
+	//
+	//		"https://www.shijuan1.com/a/sjhx9/",
+	//		"https://www.shijuan1.com/a/sjhxzk/",
+	//
+	//		"https://www.shijuan1.com/a/sjzz7/",
+	//		"https://www.shijuan1.com/a/sjzz8/",
+	//		"https://www.shijuan1.com/a/sjzz9/",
+	//		"https://www.shijuan1.com/a/sjzzzk/",
+	//
+	//		"https://www.shijuan1.com/a/sjls7/",
+	//		"https://www.shijuan1.com/a/sjls8/",
+	//		"https://www.shijuan1.com/a/sjls9/",
+	//		"https://www.shijuan1.com/a/sjlszk/",
+	//
+	//		"https://www.shijuan1.com/a/sjdl7/",
+	//		"https://www.shijuan1.com/a/sjdl8/",
+	//		"https://www.shijuan1.com/a/sjdlzk/",
+	//
+	//		"https://www.shijuan1.com/a/sjsw7/",
+	//		"https://www.shijuan1.com/a/sjsw8/",
+	//		"https://www.shijuan1.com/a/sjswzk/",
+	//	},
+	//},
+	//{
+	//	name: "高考试卷",
+	//	link: []string{
+	//		"https://www.shijuan1.com/a/sjywg1/",
+	//		"https://www.shijuan1.com/a/sjywg2/",
+	//		"https://www.shijuan1.com/a/sjywg3/",
+	//		"https://www.shijuan1.com/a/sjywgk/",
+	//
+	//		"https://www.shijuan1.com/a/sjsxg1/",
+	//		"https://www.shijuan1.com/a/sjsxg2/",
+	//		"https://www.shijuan1.com/a/sjsxg3/",
+	//		"https://www.shijuan1.com/a/sjsxgk/",
+	//
+	//		"https://www.shijuan1.com/a/sjyyg1/",
+	//		"https://www.shijuan1.com/a/sjyyg2/",
+	//		"https://www.shijuan1.com/a/sjyyg3/",
+	//		"https://www.shijuan1.com/a/sjyygk/",
+	//
+	//		"https://www.shijuan1.com/a/sjwlg1/",
+	//		"https://www.shijuan1.com/a/sjwlg2/",
+	//		"https://www.shijuan1.com/a/sjwlg3/",
+	//		"https://www.shijuan1.com/a/sjwlgk/",
+	//
+	//		"https://www.shijuan1.com/a/sjhxg1/",
+	//		"https://www.shijuan1.com/a/sjhxg2/",
+	//		"https://www.shijuan1.com/a/sjhxg3/",
+	//		"https://www.shijuan1.com/a/sjhxgk/",
+	//
+	//		"https://www.shijuan1.com/a/sjzzg1/",
+	//		"https://www.shijuan1.com/a/sjzzg2/",
+	//		"https://www.shijuan1.com/a/sjzzg3/",
+	//		"https://www.shijuan1.com/a/sjzzgk/",
+	//
+	//		"https://www.shijuan1.com/a/sjlsg1/",
+	//		"https://www.shijuan1.com/a/sjlsg2/",
+	//		"https://www.shijuan1.com/a/sjlsg3/",
+	//		"https://www.shijuan1.com/a/sjlsgk/",
+	//
+	//		"https://www.shijuan1.com/a/sjdlg1/",
+	//		"https://www.shijuan1.com/a/sjdlg2/",
+	//		"https://www.shijuan1.com/a/sjdlg3/",
+	//		"https://www.shijuan1.com/a/sjdlgk/",
+	//
+	//		"https://www.shijuan1.com/a/sjswg1/",
+	//		"https://www.shijuan1.com/a/sjswg2/",
+	//		"https://www.shijuan1.com/a/sjswg3/",
+	//		"https://www.shijuan1.com/a/sjswgk/",
+	//	},
+	//},
 }
 
 var shiJuan1SaveYear = []string{"2023", "2022", "2021", "2020", "2019", "2018", "2017", "2016", "2015"}
