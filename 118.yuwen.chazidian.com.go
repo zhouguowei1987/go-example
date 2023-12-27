9a03fef79682f87abc7a16e7f67580b3ae8aa7e6
729044963@qq.com <729044963@qq.com>
2023-12-27 15:24:06 +0800

    国家职业技能标准

--- 118.yuwen.chazidian.com.go
+++ 118.yuwen.chazidian.com.go
@@ -189,8 +189,8 @@ type YuWenChaZiDianResponse struct {
 	Data []YuWenChaZiDianResponseData `json:"Data"`
 }
 type YuWenChaZiDianResponseData struct {
-	Id          int    `json:"Id"`
-	Title       string `json:"Title"`
+	Id          int    `json:"id"`
+	Title       string `json:"title"`
 	Description string `json:"description"`
 	Url         string `json:"url"`
 	Date        string `json:"date"`
