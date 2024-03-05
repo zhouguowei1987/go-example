3f19d0414b1352d372275ee868476f5218bc9b38
zhouguowei <729044963@qq.com>
2024-03-05 13:28:11 +0800

    提交

--- 99.dbba.sacinfo.org.cn.go
+++ 99.dbba.sacinfo.org.cn.go
@@ -59,7 +59,7 @@ type ResponseDataRecords struct {
 // @Description https://dbba.sacinfo.org.cn/，获取地方标准文档
 func main() {
 	requestUrl := "https://dbba.sacinfo.org.cn/stdQueryList"
-	current := 1747
+	current := 20
 	minCurrent := 1
 	size := 15
 	status := "现行"
