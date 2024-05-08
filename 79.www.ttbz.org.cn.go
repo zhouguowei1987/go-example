46ffe59e18073f8c895b9be033826a68b6e11f1f
zhouguowei <729044963@qq.com>
2024-05-08 19:40:26 +0800

    提交

--- 79.www.ttbz.org.cn.go
+++ 79.www.ttbz.org.cn.go
@@ -17,8 +17,8 @@ import (
 // @Title 获取全国团体标准信息平台Pdf文档
 // @Description https://www.ttbz.org.cn/，将全国团体标准信息平台Pdf文档入库
 func main() {
-	var startId = 108000
-	var endId = 108153
+	var startId = 108600
+	var endId = 108667
 	goCh := make(chan int, endId-startId)
 	for id := startId; id <= endId; id++ {
 		go func(id int) {
