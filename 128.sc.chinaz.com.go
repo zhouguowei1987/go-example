9dc6b1b7b46b6255eb3806f4e8073205f90b7d2e
zhouguowei <729044963@qq.com>
2024-11-13 14:42:10 +0800

    提交

--- 128.sc.chinaz.com.go
+++ 128.sc.chinaz.com.go
@@ -6,13 +6,13 @@ import (
 	"github.com/antchfx/htmlquery"
 	"golang.org/x/net/html"
 	"io"
-	"math/rand"
 	"net"
 	"net/http"
 	"net/url"
 	"os"
 	"path/filepath"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -101,7 +101,7 @@ func ChinaZPptSetHttpProxy() (httpclient *http.Client) {
 	return httpclient
 }
 
-var ChinaZPptListCookie = "Hm_lvt_52c68b6edf9e9c811ffb29aafb14cd06=1731469471; Hm_lpvt_52c68b6edf9e9c811ffb29aafb14cd06=1731469566; Hm_lvt_398913ed58c9e7dfe9695953fb7b6799=1731469134; HMACCOUNT=2CEC63D57647BCA5; ASP.NET_SessionId=hkkpslgmloi04mxxkbm30tlj; _clck=1uyxgz2%7C2%7Cfqu%7C0%7C1778; cz_statistics_visitor=fbb756bb-351a-1934-6bac-264e556d7aeb; Hm_lvt_52c68b6edf9e9c811ffb29aafb14cd06=1731469471,1731469856; Hm_lpvt_52c68b6edf9e9c811ffb29aafb14cd06=1731469856; Hm_lpvt_398913ed58c9e7dfe9695953fb7b6799=1731472242; _clsk=1jl5zoh%7C1731472243396%7C11%7C1%7Cp.clarity.ms%2Fcollect"
+var ChinaZPpCookie = "cz_statistics_visitor=c885a4ff-3789-bf0d-cd54-0414aebde75b; Hm_lvt_398913ed58c9e7dfe9695953fb7b6799=1731474256; HMACCOUNT=487EF362690A1D5D; _clck=g33rm5%7C2%7Cfqu%7C0%7C1778; Hm_lvt_dc79411433d5171fc5e72914df433002=1731476185; Hm_lvt_ca96c3507ee04e182fb6d097cb2a1a4c=1731476983; HMACCOUNT=487EF362690A1D5D; _clsk=algv0w%7C1731478472282%7C19%7C1%7Cp.clarity.ms%2Fcollect; Hm_lpvt_ca96c3507ee04e182fb6d097cb2a1a4c=1731478524; ucvalidate=2b64f270-1efb-3ef4-d943-ec0373599f69; Access-Token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImNoaW5hel84MjQ2ODkxIiwibmFtZWlkIjoiNTM5NzE4OSIsIlRpY2tldCI6IjlhYzg3MDg5LWNkZjgtOWVkZi02MTYxLTI2ODkwNDA3MWFiYiIsIm5iZiI6MTczMTQ3ODU3OCwiZXhwIjoxNzMyMDgzMzc4LCJpYXQiOjE3MzE0Nzg1NzgsImlzcyI6Imh0dHA6Ly9tLnNjLmNoaW5hei5jb20vIiwiYXVkIjoiaHR0cDovL20uc2MuY2hpbmF6LmNvbS8ifQ.fDrjvH6oi4VWbLUddRp1yfrKV3oiTYfEUbSNbK-mxL0; Hm_lpvt_dc79411433d5171fc5e72914df433002=1731478577; CzScCookie=e51f1396-c270-2ca4-ae97-2eb3eda5dcae; Hm_lpvt_398913ed58c9e7dfe9695953fb7b6799=1731478577"
 
 var DownChinaZPptNextPageSleep = 10
 
@@ -117,13 +117,13 @@ func main() {
 			pageListUrl = fmt.Sprintf("https://m.sc.chinaz.com/ppt/?page=%d", curPage)
 		}
 		fmt.Println(pageListUrl)
-		pageListDoc, err := QueryChinaZPpt(pageListUrl, "https://m.sc.chinaz.com/")
+		pageListChinaZPpt, err := ListChinaZPpt(pageListUrl, "https://m.sc.chinaz.com/")
 		if err != nil {
 			ChinaZPptHttpProxyUrl = ""
 			fmt.Println(err)
 			continue
 		}
-		liNodes := htmlquery.Find(pageListDoc, `//div[@class="index-box"]/div[@class="ppt-list"]/div[@class="item"]`)
+		liNodes := htmlquery.Find(pageListChinaZPpt, `//div[@class="index-box"]/div[@class="ppt-list"]/div[@class="item"]`)
 		if len(liNodes) <= 0 {
 			break
 		}
@@ -133,18 +133,22 @@ func main() {
 			Title := htmlquery.SelectAttr(TitleNode, "title")
 			fmt.Println(Title)
 
-			filePath := "../m.sc.chinaz.com/ppt/" + Title + ".pdf"
+			filePath := "../sc.chinaz.com/ppt/" + Title + ".pptx"
+			fmt.Println(filePath)
 			if _, err := os.Stat(filePath); err != nil {
 				ViewChinaZPptDetailUrl := htmlquery.SelectAttr(TitleNode, "href")
+				// 转化为pc端url
+				ViewChinaZPptDetailUrl = strings.Replace(ViewChinaZPptDetailUrl, "https://m.sc.chinaz.com", "https://sc.chinaz.com", 1)
+				ViewChinaZPptDetailUrl = strings.Replace(ViewChinaZPptDetailUrl, ".html", ".htm", 1)
 				fmt.Println(ViewChinaZPptDetailUrl)
-				QueryChinaZPptDownLoadUrlDoc, err := QueryChinaZPpt(ViewChinaZPptDetailUrl, pageListUrl)
+				ViewChinaZPptDoc, err := ViewChinaZPpt(ViewChinaZPptDetailUrl, pageListUrl)
 				if err != nil {
 					ChinaZPptHttpProxyUrl = ""
 					fmt.Println(err)
 					continue
 				}
 
-				DownloadNode := htmlquery.FindOne(QueryChinaZPptDownLoadUrlDoc, `//a`)
+				DownloadNode := htmlquery.FindOne(ViewChinaZPptDoc, `//div[@class="right-div"]/div[@class="new-btn-div"]/a[@class="a-download-btn"]`)
 				if DownloadNode == nil {
 					fmt.Println("没有下载按钮，跳过")
 					continue
@@ -159,7 +163,7 @@ func main() {
 					continue
 				}
 				fmt.Println("=======下载完成========")
-				DownLoadChinaZPptTimeSleep := rand.Intn(20)
+				DownLoadChinaZPptTimeSleep := 20
 				for i := 1; i <= DownLoadChinaZPptTimeSleep; i++ {
 					time.Sleep(time.Second)
 					fmt.Println("page="+strconv.Itoa(curPage)+"===========下载", Title, "成功，暂停", DownLoadChinaZPptTimeSleep, "秒，倒计时", i, "秒===========")
@@ -174,7 +178,7 @@ func main() {
 	}
 }
 
-func QueryChinaZPpt(requestUrl string, referer string) (doc *html.Node, err error) {
+func ListChinaZPpt(requestUrl string, referer string) (doc *html.Node, err error) {
 	// 初始化客户端
 	var client *http.Client = &http.Client{
 		Transport: &http.Transport{
@@ -200,24 +204,90 @@ func QueryChinaZPpt(requestUrl string, referer string) (doc *html.Node, err erro
 		return doc, err
 	}
 
-	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
+	req.Header.Set("authority", "m.sc.chinaz.com")
+	req.Header.Set("method", "GET")
+	path := strings.Replace(requestUrl, "https://m.sc.chinaz.com", "", 1)
+	fmt.Println(path)
+	req.Header.Set("path", path)
+	req.Header.Set("scheme", "https")
+	req.Header.Set("Accept", "ext/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
 	req.Header.Set("Accept-Language", "zh-CN,zh;q=0.9")
 	req.Header.Set("Connection", "keep-alive")
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Set("Cookie", ChinaZPptListCookie)
-	req.Header.Set("Host", "m.sc.chinaz.com")
-	req.Header.Set("Origin", "https://m.sc.chinaz.com")
+	req.Header.Set("Cookie", ChinaZPpCookie)
 	req.Header.Set("Referer", referer)
-	req.Header.Set("sec-ch-ua", "\"Chromium\";v=\"110\", \"Not A(Brand\";v=\"24\", \"Google Chrome\";v=\"110\"")
-	req.Header.Set("sec-ch-ua-mobile", "?0")
-	req.Header.Set("sec-ch-ua-platform", "\"macOS\"")
-	req.Header.Set("Sec-Fetch-Dest", "empty")
-	req.Header.Set("Sec-Fetch-Mode", "cors")
-	req.Header.Set("Sec-Fetch-Site", "same-origin")
+	req.Header.Set("priority", "u=0, i")
+	req.Header.Set("sec-ch-ua", "Google Chrome\";v=\"129\", \"Not=A?Brand\";v=\"8\", \"Chromium\";v=\"129\"")
+	req.Header.Set("sec-ch-ua-mobile", "?1")
+	req.Header.Set("sec-ch-ua-platform", "\"Android\"")
+	req.Header.Set("Sec-Fetch-Dest", "document")
+	req.Header.Set("Sec-Fetch-Mode", "navigate")
+	req.Header.Set("Sec-Fetch-Site", "none")
 	req.Header.Set("Sec-Fetch-User", "?1")
 	req.Header.Set("Upgrade-Insecure-Requests", "1")
-	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36")
-	req.Header.Set("X-Requested-With", "XMLHttpRequest")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Mobile Safari/537.36")
+	resp, err := client.Do(req) //拿到返回的内容
+	if err != nil {
+		return doc, err
+	}
+	defer resp.Body.Close()
+	// 如果访问失败，就打印当前状态码
+	if resp.StatusCode != http.StatusOK {
+		return doc, errors.New("http status :" + strconv.Itoa(resp.StatusCode))
+	}
+	doc, err = htmlquery.Parse(resp.Body)
+	if err != nil {
+		return doc, err
+	}
+	return doc, nil
+}
+
+func ViewChinaZPpt(requestUrl string, referer string) (doc *html.Node, err error) {
+	// 初始化客户端
+	var client *http.Client = &http.Client{
+		Transport: &http.Transport{
+			Dial: func(netw, addr string) (net.Conn, error) {
+				c, err := net.DialTimeout(netw, addr, time.Second*3)
+				if err != nil {
+					fmt.Println("dail timeout", err)
+					return nil, err
+				}
+				return c, nil
+
+			},
+			MaxIdleConnsPerHost:   10,
+			ResponseHeaderTimeout: time.Second * 3,
+		},
+	}
+	if ChinaZPptEnableHttpProxy {
+		client = ChinaZPptSetHttpProxy()
+	}
+	req, err := http.NewRequest("GET", requestUrl, nil) //建立连接
+
+	if err != nil {
+		return doc, err
+	}
+
+	req.Header.Set("authority", "sc.chinaz.com")
+	req.Header.Set("method", "GET")
+	path := strings.Replace(requestUrl, "https://sc.chinaz.com", "", 1)
+	fmt.Println(path)
+	req.Header.Set("path", path)
+	req.Header.Set("scheme", "https")
+	req.Header.Set("Accept", "ext/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
+	req.Header.Set("Accept-Language", "zh-CN,zh;q=0.9")
+	req.Header.Set("Connection", "keep-alive")
+	req.Header.Set("Cookie", ChinaZPpCookie)
+	req.Header.Set("Referer", referer)
+	req.Header.Set("priority", "u=0, i")
+	req.Header.Set("sec-ch-ua", "Google Chrome\";v=\"129\", \"Not=A?Brand\";v=\"8\", \"Chromium\";v=\"129\"")
+	req.Header.Set("sec-ch-ua-mobile", "?1")
+	req.Header.Set("sec-ch-ua-platform", "\"Android\"")
+	req.Header.Set("Sec-Fetch-Dest", "document")
+	req.Header.Set("Sec-Fetch-Mode", "navigate")
+	req.Header.Set("Sec-Fetch-Site", "none")
+	req.Header.Set("Sec-Fetch-User", "?1")
+	req.Header.Set("Upgrade-Insecure-Requests", "1")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Mobile Safari/537.36")
 	resp, err := client.Do(req) //拿到返回的内容
 	if err != nil {
 		return doc, err
@@ -258,15 +328,19 @@ func DownLoadChinaZPpt(attachmentUrl string, referer string, filePath string) er
 	if err != nil {
 		return err
 	}
+	req.Header.Set("authority", "sc.chinaz.com")
+	req.Header.Set("method", "GET")
+	req.Header.Set("path", "/ppt/")
+	req.Header.Set("scheme", "https")
 	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
 	req.Header.Set("Accept-Encoding", "gzip, deflate")
 	req.Header.Set("Accept-Language", "zh-CN,zh;q=0.9")
 	req.Header.Set("Cache-Control", "max-age=0")
 	req.Header.Set("Connection", "keep-alive")
-	req.Header.Set("Host", "m.sc.chinaz.com")
-	req.Header.Set("Origin", "https://m.sc.chinaz.com")
+	req.Header.Set("Host", "sc.chinaz.com")
+	req.Header.Set("Origin", "https://sc.chinaz.com")
 	req.Header.Set("Referer", referer)
-	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Mobile Safari/537.36")
 	resp, err := client.Do(req) //拿到返回的内容
 	if err != nil {
 		return err
