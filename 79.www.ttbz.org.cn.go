1cb2eaa839d4b26ba5dd480ba2c092bfca2cfbd8
zhouguowei <729044963@qq.com>
2025-02-11 16:27:58 +0800

    提交

--- 79.www.ttbz.org.cn.go
+++ 79.www.ttbz.org.cn.go
@@ -171,63 +171,65 @@ func tbzSpider(id int) error {
 		aHrefText := strings.TrimSpace(htmlquery.InnerText(aHrefTdNode[1]))
 		if aHrefText != "" {
 			span2Text := htmlquery.InnerText(htmlquery.FindOne(aHrefTdNode[1], `./span[@id="Span2"]`))
-			if span2Text != "不公开" {
-				aHref := htmlquery.InnerText(htmlquery.FindOne(aHrefTdNode[1], `./span[@id="Span2"]/a`))
-				if aHref == "查看" {
-					fmt.Println(aHref)
-					// 标准编号
-					standardNoTrNode := trNodes[2]
-					standardNoTdNodes := htmlquery.Find(standardNoTrNode, `./td`)
-					standardNo := htmlquery.InnerText(htmlquery.FindOne(standardNoTdNodes[1], `./span[@id="r1_c5"]`))
-					standardNo = strings.ReplaceAll(standardNo, "/", "-")
-					fmt.Println(standardNo)
+			if span2Text == "不公开" {
+				fmt.Println("不公开文档，跳过")
+				return errors.New("不公开文档，跳过")
+			}
+			aHref := htmlquery.InnerText(htmlquery.FindOne(aHrefTdNode[1], `./span[@id="Span2"]/a`))
+			if aHref != "查看" {
+				fmt.Println("没有下载链接")
+				return errors.New("没有下载链接")
+			}
+			// 标准编号
+			standardNoTrNode := trNodes[2]
+			standardNoTdNodes := htmlquery.Find(standardNoTrNode, `./td`)
+			standardNo := htmlquery.InnerText(htmlquery.FindOne(standardNoTdNodes[1], `./span[@id="r1_c5"]`))
+			standardNo = strings.ReplaceAll(standardNo, "/", "-")
+			fmt.Println(standardNo)
 
-					// 中文标题
-					chineseTitleTrNode := trNodes[3]
-					chineseTitleTdNodes := htmlquery.Find(chineseTitleTrNode, `./td`)
-					chineseTitle := htmlquery.InnerText(htmlquery.FindOne(chineseTitleTdNodes[1], `./span[@id="r1_c5"]`))
-					chineseTitle = strings.ReplaceAll(chineseTitle, "/", "-")
-					chineseTitle = strings.ReplaceAll(chineseTitle, " ", "")
-					fmt.Println(chineseTitle)
+			// 中文标题
+			chineseTitleTrNode := trNodes[3]
+			chineseTitleTdNodes := htmlquery.Find(chineseTitleTrNode, `./td`)
+			chineseTitle := htmlquery.InnerText(htmlquery.FindOne(chineseTitleTdNodes[1], `./span[@id="r1_c5"]`))
+			chineseTitle = strings.ReplaceAll(chineseTitle, "/", "-")
+			chineseTitle = strings.ReplaceAll(chineseTitle, " ", "")
+			fmt.Println(chineseTitle)
 
-					pdfsUrl := fmt.Sprintf("https://www.ttbz.org.cn/Pdfs/Index/?ftype=st&pms=%d", id)
-					pdfsDoc, err := getTbz(pdfsUrl)
-					if err != nil {
-						return err
-					}
-					iframeSrcNode := htmlquery.FindOne(pdfsDoc, `//iframe[@id="myiframe"]/@src`)
-					iframeSrc := htmlquery.InnerText(iframeSrcNode)
-					fmt.Println(iframeSrc)
+			pdfsUrl := fmt.Sprintf("https://www.ttbz.org.cn/Pdfs/Index/?ftype=st&pms=%d", id)
+			pdfsDoc, err := getTbz(pdfsUrl)
+			if err != nil {
+				return err
+			}
+			iframeSrcNode := htmlquery.FindOne(pdfsDoc, `//iframe[@id="myiframe"]/@src`)
+			iframeSrc := htmlquery.InnerText(iframeSrcNode)
+			fmt.Println(iframeSrc)
 
-					// 下载pdf文件
-					pdfUrl := strings.ReplaceAll(iframeSrc, "/Home/PdfView?file=", "https://www.ttbz.org.cn")
-					// 移除rnd参数
-					pdfUrl = strings.Split(pdfUrl, "&")[0]
-					fmt.Println(pdfUrl)
+			// 下载pdf文件
+			pdfUrl := strings.ReplaceAll(iframeSrc, "/Home/PdfView?file=", "https://www.ttbz.org.cn")
+			// 移除rnd参数
+			pdfUrl = strings.Split(pdfUrl, "&")[0]
+			fmt.Println(pdfUrl)
 
-					filePath := "../www.ttbz.org.cn/" + strconv.Itoa(id) + "-" + chineseTitle + "(" + strings.ReplaceAll(standardNo, "T/", "T") + ")" + ".pdf"
-					if _, err := os.Stat(filePath); err != nil {
-						fmt.Println("=======开始下载========")
-						err = downloadPdf(pdfUrl, filePath)
-						if err != nil {
-							return err
-						}
-						//复制文件
-						tempFilePath := strings.ReplaceAll(filePath, "www.ttbz.org.cn", "temp-www.ttbz.org.cn")
-						err = copyTbzFile(filePath, tempFilePath)
-						if err != nil {
-							return err
-						}
-						fmt.Println("=======完成下载========")
+			filePath := "../www.ttbz.org.cn/" + strconv.Itoa(id) + "-" + chineseTitle + "(" + strings.ReplaceAll(standardNo, "T/", "T") + ")" + ".pdf"
+			if _, err := os.Stat(filePath); err != nil {
+				fmt.Println("=======开始下载========")
+				err = downloadPdf(pdfUrl, filePath)
+				if err != nil {
+					return err
+				}
+				//复制文件
+				tempFilePath := strings.ReplaceAll(filePath, "www.ttbz.org.cn", "temp-www.ttbz.org.cn")
+				err = copyTbzFile(filePath, tempFilePath)
+				if err != nil {
+					return err
+				}
+				fmt.Println("=======完成下载========")
 
-						// 设置下载倒计时
-						DownLoadTTbzTimeSleep := 10
-						for i := 1; i <= DownLoadTTbzTimeSleep; i++ {
-							time.Sleep(time.Second)
-							fmt.Println("id="+strconv.Itoa(id)+"===========操作完成，", "暂停", DownLoadTTbzTimeSleep, "秒，倒计时", i, "秒===========")
-						}
-					}
-
+				// 设置下载倒计时
+				DownLoadTTbzTimeSleep := 10
+				for i := 1; i <= DownLoadTTbzTimeSleep; i++ {
+					time.Sleep(time.Second)
+					fmt.Println("id="+strconv.Itoa(id)+"===========操作完成，", "暂停", DownLoadTTbzTimeSleep, "秒，倒计时", i, "秒===========")
 				}
 			}
 		}
