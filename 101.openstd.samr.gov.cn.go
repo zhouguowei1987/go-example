8fd12052a1dfd2dbeabe63a7210d71002fe2cbbd
zhouguowei <729044963@qq.com>
2025-04-10 14:59:00 +0800

    提交

--- 101.openstd.samr.gov.cn.go
+++ 101.openstd.samr.gov.cn.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/antchfx/htmlquery"
+	"github.com/otiai10/gosseract/v2"
 	"io"
 	"net"
 	"net/http"
@@ -11,6 +12,7 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -33,6 +35,8 @@ type StdCategory struct {
 	StdName string
 	StdUrl  string
 }
+
+const OpenStdCookie = "JSESSIONID=039B1977E76E2A76D823BCBB082D9503; Hm_lvt_50758913e6f0dfc9deacbfebce3637e4=1725936996; Hm_lpvt_50758913e6f0dfc9deacbfebce3637e4=1725936996; HMACCOUNT=487EF362690A1D5D; _yfx_session_10000005=%7B%22_yfx_firsttime%22%3A%221708918785493%22%2C%22_yfx_lasttime%22%3A%221740208275353%22%2C%22_yfx_visittime%22%3A%221740208275353%22%2C%22_yfx_domidgroup%22%3A%221740208275353%22%2C%22_yfx_domallsize%22%3A%22100%22%2C%22_yfx_cookie%22%3A%2220240226113945497390226550665182%22%2C%22_yfx_lastvisittime%22%3A%221740208275353%22%2C%22_yfx_returncount%22%3A%222%22%7D"
 
 // ychEduSpider 获取国家标准文档
 // @Title 获取国家标准文档
@@ -82,6 +86,28 @@ func main() {
 				HCno = HCno[10 : len(HCno)-3]
 				fmt.Println(HCno)
 
+				// 获取验证码图片
+				// 获取当前时间的纳秒级时间戳
+				nanoTimestamp := time.Now().UnixNano()
+				// 将纳秒级时间戳转换为毫秒级
+				millis := nanoTimestamp / 1e6 // 或者 nanoTimestamp / 1000000
+				fmt.Println("当前时间的毫秒级时间戳:", millis)
+				validateCodeUrl := fmt.Sprintf("http://c.gb688.cn/bzgk/gb/gc?_%d", millis)
+				fmt.Println(validateCodeUrl)
+
+				validateCodeFilePath := "./openstd-validate-code/validate-code.png"
+				validateCodeOpenStdReferer := fmt.Sprintf("http://c.gb688.cn/bzgk/gb/showGb?type=download&hcno=%s", HCno)
+				err := downloadValidateCodeOpenStd(validateCodeUrl, validateCodeOpenStdReferer, validateCodeFilePath)
+				if err != nil {
+					fmt.Println(err)
+					continue
+				}
+
+				// 获取验证码文字信息
+				captcha, err := TesseractValidateCodeOpenStd(validateCodeFilePath)
+				captcha = strings.TrimSpace(captcha)
+				fmt.Println("识别的验证码：", captcha)
+
 				// 详情URL
 				detailUrl := fmt.Sprintf("http://c.gb688.cn/bzgk/gb/showGb?type=download&hcno=%s", HCno)
 				fmt.Println(detailUrl)
@@ -106,6 +132,89 @@ func main() {
 			page++
 		}
 	}
+}
+
+func downloadValidateCodeOpenStd(validateCodeUrl string, referer string, filePath string) error {
+	// 初始化客户端
+	var client *http.Client = &http.Client{
+		Transport: &http.Transport{
+			Dial: func(netw, addr string) (net.Conn, error) {
+				c, err := net.DialTimeout(netw, addr, time.Second*3)
+				if err != nil {
+					fmt.Println("dail timeout", err)
+					return nil, err
+				}
+				return c, nil
+
+			},
+			MaxIdleConnsPerHost:   10,
+			ResponseHeaderTimeout: time.Second * 3,
+		},
+	}
+	if OPenStdEnableHttpProxy {
+		client = OPenStdSetHttpProxy()
+	}
+	req, err := http.NewRequest("GET", validateCodeUrl, nil) //建立连接
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Accept", "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8")
+	req.Header.Set("Accept-Encoding", "gzip, deflate, br")
+	req.Header.Set("Accept-Language", "zh-CN,zh;q=0.9")
+	req.Header.Set("Cookie", OpenStdCookie)
+	req.Header.Set("Connection", "keep-alive")
+	req.Header.Set("Host", "c.gb688.cn")
+	req.Header.Set("Referer", referer)
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36")
+	resp, err := client.Do(req) //拿到返回的内容
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	// 如果访问失败，就打印当前状态码
+	if resp.StatusCode != http.StatusOK {
+		return errors.New("http status :" + strconv.Itoa(resp.StatusCode))
+	}
+
+	// 创建一个文件用于保存
+	fileDiv := filepath.Dir(filePath)
+	if _, err = os.Stat(fileDiv); err != nil {
+		if os.MkdirAll(fileDiv, 0777) != nil {
+			return err
+		}
+	}
+	out, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// 然后将响应流和文件流对接起来
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func TesseractValidateCodeOpenStd(imagePath string) (codeText string, err error) {
+	// 创建Tesseract客户端
+	client := gosseract.NewClient()
+	defer client.Close()
+	// 设置语言模型
+	client.SetLanguage("eng")
+	// 设置白名单字符
+	client.SetWhitelist("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	// 识别图片中的文本
+	err = client.SetImage(imagePath)
+	if err != nil {
+		return codeText, fmt.Errorf("设置图片出错: %v", err)
+	}
+	text, err := client.Text()
+	if err != nil {
+		return codeText, fmt.Errorf("识别出错: %v", err)
+	}
+	return text, nil
 }
 
 func downloadOPenStd(attachmentUrl string, referer string, filePath string) error {
--- go.mod
+++ go.mod
@@ -11,4 +11,9 @@ require (
 	rsc.io/pdf v0.1.1
 )
 
-require github.com/otiai10/gosseract/v2 v2.4.1 // indirect
+require (
+	github.com/disintegration/imaging v1.6.2 // indirect
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
+	github.com/otiai10/gosseract/v2 v2.4.1 // indirect
+	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
+)
