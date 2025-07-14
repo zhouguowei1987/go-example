1ba0c9e160862ceb8bb09898d7491349c54fa581
zhouguowei1987 <729044963@qq.com>
2025-07-14 23:28:18 +0800

    提交

--- 136.sppt.cfsa.net.cn.go
+++ 136.sppt.cfsa.net.cn.go
@@ -1,20 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net"
 	"net/http"
 	"net/url"
 	"os"
+	_ "os"
 	"path/filepath"
+	_ "path/filepath"
 	"strconv"
 	"strings"
 	"time"
 
 	"github.com/antchfx/htmlquery"
-	"golang.org/x/net/html"
+	_ "golang.org/x/net/html"
 )
 
 var SpPtCfSaEnableHttpProxy = false
@@ -103,10 +107,19 @@ func SpPtCfSaSetHttpProxy() (httpclient *http.Client) {
 }
 
 type QuerySpPtCfSaListFormData struct {
-	isLength  int
-	num_tn int
-	standard_type   string
+	isLength      int
+	num_tn        int
+	standard_type string
 	keyword       string
+}
+
+type DownloadSpPtCfSaFormData struct {
+	task       string
+	accessData string
+	bzlb       string
+	fact_name  string
+	file_guid  string
+	keyword    string
 }
 
 var SpPtCfSaCookie = "name=value; cookieName=cookieValue; JSESSIONID=1939457719D7E496EDAAB20F581FA15F"
@@ -116,35 +129,81 @@ var SpPtCfSaCookie = "name=value; cookieName=cookieValue; JSESSIONID=1939457719D
 // @Description https://sppt.cfsa.net.cn:8086/db/，下载食品安全国家标准数据文档
 func main() {
 	pageListUrl := "https://sppt.cfsa.net.cn:8086/db?task=indexSearch"
-    fmt.Println(pageListUrl)
-    querySpPtCfSaListFormData := QuerySpPtCfSaListFormData{
-        isLength:  9999,
-        num_tn: 2,
-        standard_type:    "",
-        keyword:  "",
-    }
-    querySpPtCfSaListResponse, err := QuerySpPtCfSaList(pageListUrl, querySpPtCfSaListFormData)
-    if err != nil {
-        SpPtCfSaHttpProxyUrl = ""
-        fmt.Println(err)
-        continue
-    }
-    fmt.Println(querySpPtCfSaListResponse)
-}
+	fmt.Println(pageListUrl)
+	querySpPtCfSaListFormData := QuerySpPtCfSaListFormData{
+		isLength:      9999,
+		num_tn:        2,
+		standard_type: "",
+		keyword:       "",
+	}
+	querySpPtCfSaListResponse, err := QuerySpPtCfSaList(pageListUrl, querySpPtCfSaListFormData)
+	if err != nil {
+		SpPtCfSaHttpProxyUrl = ""
+		fmt.Println(err)
+	}
+	for _, spPtCfSa := range querySpPtCfSaListResponse {
+		fmt.Println("=====================開始處理數據=========================")
+		code := spPtCfSa.CODE
+		fmt.Println(code)
 
-type QuerySpPtCfSaListFJ struct {
-	FACT_NAME    string   `json:"FACT_NAME"`
-	ID_F    string   `json:"ID_F"`
+		title := spPtCfSa.TITLE
+		title = strings.TrimSpace(title)
+		title = strings.ReplaceAll(title, "-", "")
+		title = strings.ReplaceAll(title, " ", "")
+		title = strings.ReplaceAll(title, "|", "-")
+		fmt.Println(title)
+
+		id_f := spPtCfSa.FJ[0].ID_F
+		fmt.Println(id_f)
+
+		filePath := "../sppt.cfsa.net.cn/" + title + "(" + code + ")" + ".pdf"
+		fmt.Println(filePath)
+
+		_, err = os.Stat(filePath)
+		if err == nil {
+			fmt.Println("文档已下载过，跳过")
+			continue
+		}
+
+		fmt.Println("=======开始下载========")
+
+		downloadSpPtCfSaUrl := "https://sppt.cfsa.net.cn:8086/cfsa_aiguo"
+		fmt.Println(downloadSpPtCfSaUrl)
+		downloadSpPtCfSaFormData := DownloadSpPtCfSaFormData{
+			task:       "d_p",
+			accessData: "gj",
+			bzlb:       "",
+			fact_name:  "",
+			file_guid:  id_f,
+			keyword:    "",
+		}
+		err := downloadSpPtCfSa(downloadSpPtCfSaUrl, downloadSpPtCfSaFormData, filePath)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		fmt.Println("=======完成下载========")
+		DownLoadSpPtCfSaTimeSleep := 10
+		for i := 1; i <= DownLoadSpPtCfSaTimeSleep; i++ {
+			time.Sleep(time.Second)
+			fmt.Println("title="+title+"===========下载", title, "成功，暂停", DownLoadSpPtCfSaTimeSleep, "秒，倒计时", i, "秒===========")
+		}
+	}
 }
 
 type QuerySpPtCfSaListResponse struct {
-	CODE    string   `json:"CODE"`
-	FJ    []QuerySpPtCfSaListFJ   `json:"FJ"`
-	ID        string `json:"ID"`
-	PDATE       string  `json:"PDATE"`
-	SSRQ       string  `json:"SSRQ"`
-	TABLENAME  string    `json:"TABLENAME"`
-	TITLE string    `json:"TITLE"`
+	CODE      string                `json:"CODE"`
+	FJ        []QuerySpPtCfSaListFJ `json:"FJ"`
+	ID        string                `json:"ID"`
+	PDATE     string                `json:"PDATE"`
+	SSRQ      string                `json:"SSRQ"`
+	TABLENAME string                `json:"TABLENAME"`
+	TITLE     string                `json:"TITLE"`
+}
+
+type QuerySpPtCfSaListFJ struct {
+	FACT_NAME string `json:"FACT_NAME"`
+	ID_F      string `json:"ID_F"`
 }
 
 func QuerySpPtCfSaList(requestUrl string, querySpPtCfSaListFormData QuerySpPtCfSaListFormData) (querySpPtCfSaListResponse []QuerySpPtCfSaListResponse, err error) {
@@ -169,31 +228,24 @@ func QuerySpPtCfSaList(requestUrl string, querySpPtCfSaListFormData QuerySpPtCfS
 	}
 	postData := url.Values{}
 	postData.Add("isLength", strconv.Itoa(querySpPtCfSaListFormData.isLength))
-	postData.Add("num_tn", querySpPtCfSaListFormData.num_tn)
+	postData.Add("num_tn", strconv.Itoa(querySpPtCfSaListFormData.num_tn))
 	postData.Add("standard_type", querySpPtCfSaListFormData.standard_type)
 	postData.Add("keyword", querySpPtCfSaListFormData.keyword)
 	req, err := http.NewRequest("POST", requestUrl, strings.NewReader(postData.Encode())) //建立连接
 
+	querySpPtCfSaListResponse = []QuerySpPtCfSaListResponse{}
 	if err != nil {
-		return doc, err
+		return querySpPtCfSaListResponse, err
 	}
 
-	req.Header.Set("Accept", "application/json, text/plain, */*")
-	//req.Header.Set("accept-encoding", "gzip, deflate, br")
+	req.Header.Set("Accept", "*/*")
 	req.Header.Set("Accept-Language", "zh-CN,zh;q=0.9")
-	req.Header.Set("Content-Type", w.FormDataContentType())
+	req.Header.Set("Connection", "keep-alive")
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
 	req.Header.Set("Cookie", SpPtCfSaCookie)
 	req.Header.Set("Host", "sppt.cfsa.net.cn:8086")
 	req.Header.Set("Origin", "https://sppt.cfsa.net.cn:8086")
 	req.Header.Set("Referer", "https://sppt.cfsa.net.cn:8086/db")
-	req.Header.Set("sec-ch-ua", "\"Chromium\";v=\"110\", \"Not A(Brand\";v=\"24\", \"Google Chrome\";v=\"110\"")
-	req.Header.Set("sec-ch-ua-mobile", "?0")
-	req.Header.Set("sec-ch-ua-platform", "\"macOS\"")
-	req.Header.Set("Sec-Fetch-Dest", "empty")
-	req.Header.Set("Sec-Fetch-Mode", "cors")
-	req.Header.Set("Sec-Fetch-Site", "same-origin")
-	req.Header.Set("Sec-Fetch-User", "?1")
-	req.Header.Set("Upgrade-Insecure-Requests", "1")
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36")
 	req.Header.Set("X-Requested-With", "XMLHttpRequest")
 	resp, err := client.Do(req) //拿到返回的内容
@@ -202,20 +254,22 @@ func QuerySpPtCfSaList(requestUrl string, querySpPtCfSaListFormData QuerySpPtCfS
 		return querySpPtCfSaListResponse, err
 	}
 	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return querySpPtCfSaListResponse, errors.New("http status :" + strconv.Itoa(resp.StatusCode))
+	}
 	respBytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println(err)
 		return querySpPtCfSaListResponse, err
 	}
-	err = json.Unmarshal(respBytes, querySpPtCfSaListResponse)
+	err = json.Unmarshal(respBytes, &querySpPtCfSaListResponse)
 	if err != nil {
-		fmt.Println(err)
 		return querySpPtCfSaListResponse, err
 	}
 	return querySpPtCfSaListResponse, nil
 }
 
-func QuerySpPtCfSaDownLoadUrl(requestUrl string) (doc *html.Node, err error) {
+func downloadSpPtCfSa(requestUrl string, downloadSpPtCfSaFormData DownloadSpPtCfSaFormData, filePath string) error {
 	// 初始化客户端
 	var client *http.Client = &http.Client{
 		Transport: &http.Transport{
@@ -235,79 +289,27 @@ func QuerySpPtCfSaDownLoadUrl(requestUrl string) (doc *html.Node, err error) {
 	if SpPtCfSaEnableHttpProxy {
 		client = SpPtCfSaSetHttpProxy()
 	}
-	req, err := http.NewRequest("GET", requestUrl, nil) //建立连接
-
-	if err != nil {
-		return doc, err
-	}
-
-	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
-	req.Header.Set("Accept-Language", "zh-CN,zh;q=0.9")
-	req.Header.Set("Connection", "keep-alive")
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Set("Cookie", DownLoadUrlCookie)
-	req.Header.Set("Host", "sppt.cfsa.net.cn:8086/db")
-	req.Header.Set("Origin", "https://sppt.cfsa.net.cn:8086/db")
-	req.Header.Set("Referer", "https://sppt.cfsa.net.cn:8086/db/uc/doc_manager.php?act=doc_list&state=all")
-	req.Header.Set("sec-ch-ua", "\"Chromium\";v=\"110\", \"Not A(Brand\";v=\"24\", \"Google Chrome\";v=\"110\"")
-	req.Header.Set("sec-ch-ua-mobile", "?0")
-	req.Header.Set("sec-ch-ua-platform", "\"macOS\"")
-	req.Header.Set("Sec-Fetch-Dest", "empty")
-	req.Header.Set("Sec-Fetch-Mode", "cors")
-	req.Header.Set("Sec-Fetch-Site", "same-origin")
-	req.Header.Set("Sec-Fetch-User", "?1")
-	req.Header.Set("Upgrade-Insecure-Requests", "1")
-	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36")
-	req.Header.Set("X-Requested-With", "XMLHttpRequest")
-	resp, err := client.Do(req) //拿到返回的内容
-	if err != nil {
-		return doc, err
-	}
-	defer resp.Body.Close()
-	// 如果访问失败，就打印当前状态码
-	if resp.StatusCode != http.StatusOK {
-		return doc, errors.New("http status :" + strconv.Itoa(resp.StatusCode))
-	}
-	doc, err = htmlquery.Parse(resp.Body)
-	if err != nil {
-		return doc, err
-	}
-	return doc, nil
-}
-
-func DownLoadDoc88(attachmentUrl string, referer string, filePath string) error {
-	// 初始化客户端
-	var client *http.Client = &http.Client{
-		Transport: &http.Transport{
-			Dial: func(netw, addr string) (net.Conn, error) {
-				c, err := net.DialTimeout(netw, addr, time.Second*3)
-				if err != nil {
-					fmt.Println("dail timeout", err)
-					return nil, err
-				}
-				return c, nil
-
-			},
-			MaxIdleConnsPerHost:   10,
-			ResponseHeaderTimeout: time.Second * 3,
-		},
-	}
-	if SpPtCfSaEnableHttpProxy {
-		client = SpPtCfSaSetHttpProxy()
-	}
-	req, err := http.NewRequest("GET", attachmentUrl, nil) //建立连接
+	postData := url.Values{}
+	postData.Add("task", downloadSpPtCfSaFormData.task)
+	postData.Add("accessData", downloadSpPtCfSaFormData.accessData)
+	postData.Add("bzlb", downloadSpPtCfSaFormData.bzlb)
+	postData.Add("fact_name", downloadSpPtCfSaFormData.fact_name)
+	postData.Add("file_guid", downloadSpPtCfSaFormData.file_guid)
+	postData.Add("keyword", downloadSpPtCfSaFormData.keyword)
+	req, err := http.NewRequest("POST", requestUrl, strings.NewReader(postData.Encode())) //建立连接
 	if err != nil {
 		return err
 	}
-	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
-	req.Header.Set("Accept-Encoding", "gzip, deflate")
+	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
 	req.Header.Set("Accept-Language", "zh-CN,zh;q=0.9")
-	req.Header.Set("Cache-Control", "max-age=0")
 	req.Header.Set("Connection", "keep-alive")
-	req.Header.Set("Host", "sppt.cfsa.net.cn:8086/db")
-	req.Header.Set("Origin", "https://sppt.cfsa.net.cn:8086/db")
-	req.Header.Set("Referer", referer)
-	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36")
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+	req.Header.Set("Cookie", SpPtCfSaCookie)
+	req.Header.Set("Host", "sppt.cfsa.net.cn:8086")
+	req.Header.Set("Origin", "https://sppt.cfsa.net.cn:8086")
+	req.Header.Set("Referer", "https://sppt.cfsa.net.cn:8086/db")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36")
+	req.Header.Set("X-Requested-With", "XMLHttpRequest")
 	resp, err := client.Do(req) //拿到返回的内容
 	if err != nil {
 		return err
