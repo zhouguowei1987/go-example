761284bc97fa52e214c6a8fca90df4ea990d9ce5
zhouguowei <729044963@qq.com>
2026-06-28 07:56:35 +0800

    提交

--- 101.openstd.samr.gov.cn.go
+++ 101.openstd.samr.gov.cn.go
@@ -35,6 +35,8 @@ func OPenStdSetHttpProxy() (httpclient *http.Client) {
 type StdCategory struct {
 	StdName string
 	StdUrl  string
+	Page  int
+	MaxPage  int
 }
 
 var OPenStdCookie = ""
@@ -47,20 +49,31 @@ func main() {
 		{
 			StdName: "强制性国家标准",
 			StdUrl:  "https://openstd.samr.gov.cn/bzgk/std/std_list_type?p.p1=1&p.p90=circulation_date&p.p91=desc",
+			Page: 10,
+			MaxPage: 612,
 		},
 		{
 			StdName: "推荐性国家标准",
 			StdUrl:  "https://openstd.samr.gov.cn/bzgk/std/std_list_type?p.p1=2&p.p90=circulation_date&p.p91=desc",
+			Page: 1,
+			MaxPage: 6037,
 		},
 		{
 			StdName: "指导性技术文件",
 			StdUrl:  "https://openstd.samr.gov.cn/bzgk/std/std_list_type?p.p1=3&p.p90=circulation_date&p.p91=desc",
+			Page: 1,
+			MaxPage: 83,
 		},
 	}
 	for _, std := range StdCategories {
-		page := 1
+		page := std.Page
 		pageSize := 10
-		for {
+		isPageListGo := true
+		for isPageListGo {
+            if page > std.MaxPage {
+                isPageListGo = false
+                break
+            }
 			pageListUrl := fmt.Sprintf(std.StdUrl+"&r=0.20175458803007884&page=%d&pageSize=%d", page, pageSize)
 			pageListRefererUrl := std.StdUrl
 			if page > 1{
@@ -71,101 +84,108 @@ func main() {
 			OPenStdCookie = ""
 			pageListDoc, err := oPenStdHtml(pageListUrl, pageListRefererUrl)
 			if err != nil {
-				page = 1
 				fmt.Println(err)
 				break
 			}
 			trNodes := htmlquery.Find(pageListDoc, `//table[@class="table result_list table-striped table-hover"]/tbody[2]/tr`)
-			if len(trNodes) <= 0 {
-				page = 1
-				break
+			if len(trNodes) > 0 {
+				for _, trNode := range trNodes {
+                    StdNoA := htmlquery.FindOne(trNode, `./td[2]/a`)
+                    StdNo := htmlquery.InnerText(StdNoA)
+                    fmt.Println(StdNo)
+
+                    StdNameA := htmlquery.FindOne(trNode, `./td[4]/a`)
+                    StdName := htmlquery.InnerText(StdNameA)
+                    StdName = strings.TrimSpace(StdName)
+                    StdName = strings.ReplaceAll(StdName, " ", "")
+                    StdName = strings.ReplaceAll(StdName, "　", "")
+                    StdName = strings.ReplaceAll(StdName, "/", "-")
+                    StdName = strings.ReplaceAll(StdName, "：", "-")
+                    StdName = strings.ReplaceAll(StdName, "《", "")
+                    StdName = strings.ReplaceAll(StdName, "》", "")
+                    StdName = strings.ReplaceAll(StdName, "--", "-")
+                    StdName = strings.ReplaceAll(StdName, "——", "-")
+                    fmt.Println(StdName)
+
+                    HCno := htmlquery.SelectAttr(StdNameA, "onclick")
+                    HCno = HCno[10 : len(HCno)-3]
+                    fmt.Println(HCno)
+
+                    // 详情URL
+                    detailUrl := fmt.Sprintf("https://openstd.samr.gov.cn/bzgk/std/newGbInfo?hcno=%s", HCno)
+                    fmt.Println(detailUrl)
+                    detailDoc, err := oPenStdHtml(detailUrl, pageListUrl)
+                    if err != nil {
+                        fmt.Println(err)
+                        continue
+                    }
+                    // 查看是否有下载按钮
+                    xzBtnNode := htmlquery.FindOne(detailDoc, `//html/body/div[3]/div/div/div/div/table[2]/tbody/tr[4]/td/button[@class="btn xz_btn btn-sm btn-warning app-hide"]`)
+                    if xzBtnNode == nil{
+                        fmt.Println("没有下载按钮，跳过")
+                        continue
+                    }
+
+                    // 下载文档URL
+                    downLoadUrl := fmt.Sprintf("https://openstd.samr.gov.cn/bzgk/std/viewGb?hcno=%s", HCno)
+                    fmt.Println(downLoadUrl)
+
+                    refererUrl := fmt.Sprintf("https://openstd.samr.gov.cn/bzgk/std/showGb?type=download&hcno=%s&request_locale=zh", HCno)
+                    fmt.Println(refererUrl)
+
+                    filePath := "../openstd.samr.gov.cn/" + StdName + "(" + StdNo + ")" + ".pdf"
+                    _, err = os.Stat(filePath)
+                    if err == nil {
+                        fmt.Println("文档已下载过，跳过")
+                        continue
+                    }
+                    fmt.Println("=======开始下载========")
+                    // 下载详情页面和文件页面要同时请求，才可以下载温年
+                    _, err = oPenStdHtml(refererUrl, detailUrl)
+                    err = downloadOPenStd(downLoadUrl, refererUrl, filePath)
+                    if err != nil {
+                        fmt.Println(err)
+                        continue
+                    }
+                    // 查看文件大小，如果是空文件，则删除
+                    fileInfo, err := os.Stat(filePath)
+                    if err == nil && fileInfo.Size() == 0{
+                        fmt.Println("空文件删除")
+                        err = os.Remove(filePath)
+                    }
+                    if err != nil {
+                        continue
+                    }
+    //                 //复制文件
+    //                 tempFilePath := strings.ReplaceAll(filePath, "openstd.samr.gov.cn", "temp-hbba.sacinfo.org.cn")
+    //                 err = copyOPenStdFile(filePath, tempFilePath)
+    //                 if err != nil {
+    //                     fmt.Println(err)
+    //                     continue
+    //                 }
+                    fmt.Println("=======开始完成========")
+                    // 设置倒计时
+                    DownLoadOPenStdTimeSleep := rand.Intn(80)
+                    if DownLoadOPenStdTimeSleep <= 60{
+                        DownLoadOPenStdTimeSleep = 60 + rand.Intn(30)
+                    }
+                    for i := 1; i <= DownLoadOPenStdTimeSleep; i++ {
+                        time.Sleep(time.Second)
+                        fmt.Println("page = "+strconv.Itoa(page)+"===StdName="+StdName+"===========操作完成，", "暂停", DownLoadOPenStdTimeSleep, "秒，倒计时", i, "秒===========")
+                    }
+                }
 			}
-			for _, trNode := range trNodes {
-				StdNoA := htmlquery.FindOne(trNode, `./td[2]/a`)
-				StdNo := htmlquery.InnerText(StdNoA)
-				fmt.Println(StdNo)
-
-				StdNameA := htmlquery.FindOne(trNode, `./td[4]/a`)
-				StdName := htmlquery.InnerText(StdNameA)
-				StdName = strings.TrimSpace(StdName)
-                StdName = strings.ReplaceAll(StdName, " ", "")
-                StdName = strings.ReplaceAll(StdName, "　", "")
-                StdName = strings.ReplaceAll(StdName, "/", "-")
-                StdName = strings.ReplaceAll(StdName, "：", "-")
-                StdName = strings.ReplaceAll(StdName, "《", "")
-                StdName = strings.ReplaceAll(StdName, "》", "")
-                StdName = strings.ReplaceAll(StdName, "--", "-")
-                StdName = strings.ReplaceAll(StdName, "——", "-")
-				fmt.Println(StdName)
-
-				HCno := htmlquery.SelectAttr(StdNameA, "onclick")
-				HCno = HCno[10 : len(HCno)-3]
-				fmt.Println(HCno)
-
-				// 详情URL
-				detailUrl := fmt.Sprintf("https://openstd.samr.gov.cn/bzgk/std/newGbInfo?hcno=%s", HCno)
-				fmt.Println(detailUrl)
-				detailDoc, err := oPenStdHtml(detailUrl, pageListUrl)
-                if err != nil {
-                    fmt.Println(err)
-                    continue
-                }
-                // 查看是否有下载按钮
-                xzBtnNode := htmlquery.FindOne(detailDoc, `//html/body/div[3]/div/div/div/div/table[2]/tbody/tr[4]/td/button[@class="btn xz_btn btn-sm btn-warning app-hide"]`)
-                if xzBtnNode == nil{
-                    fmt.Println("没有下载按钮，跳过")
-                    continue
-                }
-
-				// 下载文档URL
-				downLoadUrl := fmt.Sprintf("https://openstd.samr.gov.cn/bzgk/std/viewGb?hcno=%s", HCno)
-				fmt.Println(downLoadUrl)
-
-				refererUrl := fmt.Sprintf("https://openstd.samr.gov.cn/bzgk/std/showGb?type=download&hcno=%s&request_locale=zh", HCno)
-				fmt.Println(refererUrl)
-
-				filePath := "../openstd.samr.gov.cn/" + StdName + "(" + StdNo + ")" + ".pdf"
-				_, err = os.Stat(filePath)
-				if err == nil {
-					fmt.Println("文档已下载过，跳过")
-					continue
-				}
-				fmt.Println("=======开始下载========")
-				// 下载详情页面和文件页面要同时请求，才可以下载温年
-				_, err = oPenStdHtml(refererUrl, detailUrl)
-                err = downloadOPenStd(downLoadUrl, refererUrl, filePath)
-                if err != nil {
-                    fmt.Println(err)
-                    continue
-                }
-                // 查看文件大小，如果是空文件，则删除
-                fileInfo, err := os.Stat(filePath)
-                if err == nil && fileInfo.Size() == 0{
-                    fmt.Println("空文件删除")
-                    err = os.Remove(filePath)
-                }
-                if err != nil {
-                    continue
-                }
-                //复制文件
-                tempFilePath := strings.ReplaceAll(filePath, "openstd.samr.gov.cn", "temp-hbba.sacinfo.org.cn")
-                err = copyOPenStdFile(filePath, tempFilePath)
-                if err != nil {
-                    fmt.Println(err)
-                    continue
-                }
-                fmt.Println("=======开始完成========")
-				// 设置倒计时
-				DownLoadOPenStdTimeSleep := rand.Intn(80)
-				if DownLoadOPenStdTimeSleep <= 60{
-				    DownLoadOPenStdTimeSleep = 60 + rand.Intn(30)
-				}
-				for i := 1; i <= DownLoadOPenStdTimeSleep; i++ {
-					time.Sleep(time.Second)
-					fmt.Println("page = "+strconv.Itoa(page)+"===StdName="+StdName+"===========操作完成，", "暂停", DownLoadOPenStdTimeSleep, "秒，倒计时", i, "秒===========")
-				}
-			}
+			DownLoadOPenStdPageTimeSleep := 10
+            // DownLoadOPenStdPageTimeSleep := rand.Intn(5)
+            for i := 1; i <= DownLoadOPenStdPageTimeSleep; i++ {
+                time.Sleep(time.Second)
+                fmt.Println("page="+strconv.Itoa(page)+"=========== 暂停", DownLoadOPenStdPageTimeSleep, "秒 倒计时", i, "秒===========")
+            }
 			page++
+			if page > std.MaxPage {
+                isPageListGo = false
+                break
+            }
 		}
 	}
 }
