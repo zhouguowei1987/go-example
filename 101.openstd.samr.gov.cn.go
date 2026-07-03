b3e64001ef5a5963f2ade23ec9de3819d2538687
zhouguowei <729044963@qq.com>
2026-07-03 09:09:29 +0800

    提交

--- 101.openstd.samr.gov.cn.go
+++ 101.openstd.samr.gov.cn.go
@@ -49,19 +49,19 @@ func main() {
 // 		{
 // 			StdName: "强制性国家标准",
 // 			StdUrl:  "https://openstd.samr.gov.cn/bzgk/std/std_list_type?p.p1=1&p.p90=circulation_date&p.p91=desc",
-// 			Page: 133,
+// 			Page: 248,
 // 			MaxPage: 612,
 // 		},
 		{
 			StdName: "推荐性国家标准",
 			StdUrl:  "https://openstd.samr.gov.cn/bzgk/std/std_list_type?p.p1=2&p.p90=circulation_date&p.p91=desc",
-			Page: 173,
+			Page: 227,
 			MaxPage: 6037,
 		},
 // 		{
 // 			StdName: "指导性技术文件",
 // 			StdUrl:  "https://openstd.samr.gov.cn/bzgk/std/std_list_type?p.p1=3&p.p90=circulation_date&p.p91=desc",
-// 			Page: 55,
+// 			Page: 1,
 // 			MaxPage: 83,
 // 		},
 	}
